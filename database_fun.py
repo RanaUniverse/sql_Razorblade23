20cfc056afc2c1418361e7db5cfaa38f33a5512b
Rana Universe <142967497+RanaUniverse@users.noreply.github.com>
2024-04-25 06:35:06 +0530

    add three fun for get id_ and return the row 2ndly update user row, 3rdly row count
    see theree fun has beeen addd to make my life easy

--- database_fun.py
+++ database_fun.py
@@ -80,6 +80,93 @@ def add_user_and_return_id_(
 
 
 
+def find_user_from_id_(id_:int = None):
+    '''
+    id_ is the primary column value
+    this will take unique id_ and then it will search the row
+    '''
+    if id_ is None:
+        print("Please provide valid id_ other than 'None'")
+        return None
+
+    try:
+        user_obj = session.query(UserDetails).filter_by(id_=id_).first()
+        print(user_obj)
+        return user_obj
+    except Exception as e:
+        print("A error has came:", e)
+        return None
+
+
+
+
+def update_user_by_id(id_, **kwargs):
+    '''
+    Here the columns names are,
+    id_         INTEGER  NOT NULL,
+    user_id_    INTEGER,
+    username_   VARCHAR,
+    full_name_  VARCHAR,
+    type_       VARCHAR,
+    token_      INTEGER,
+    start_time_ DATETIME,
+    end_time_   DATETIME,
+    PRIMARY KEY (
+        id_
+    )
+    so i need to kwargs inside this so that i can chagne approgpriag
+    '''
+    try:
+        print("before update", find_user_from_id_(id_))
+        update_query = (
+            session.query(UserDetails)
+            .filter(UserDetails.id_ == id_)
+            .update(kwargs)
+        )
+
+        session.commit()
+
+        if update_query:
+            print(f"User with id_ {id_} updated successfully.")
+            return True
+        else:
+            print(f"No user found with id_ {id_}.")
+            return False
+
+    except Exception as e:
+        session.rollback()
+        print("Error updating user:", e)
+        return False
+    finally:
+        session.close()
+        print("AFter update", find_user_from_id_(id_))
+
+
+
+
+
+
+
+def total_row_count():
+    '''
+    Need to impliment later ⚠⚠⚠🍌🍌🍌
+    This will count how many users are there in the table
+    it will count how many row is there and return this int
+    '''
+    def long_time_taken():
+        user_obj = session.query(UserDetails).all()
+        print(len(user_obj))
+        return len(user_obj)
+
+    user_obj = session.query(UserDetails).count()
+    print(user_obj)
+    return user_obj
+
+
+
+
+
+
 
 
 
